c3a80f7bb5f30d46bb06d4dbe9a801fa2d126490
Walid <bezouiw@gmail.com>
2025-10-21 21:39:46 +0000

    not all texts font are updated for arabic

--- src/app/creators/join/page.tsx
+++ src/app/creators/join/page.tsx
@@ -4,9 +4,14 @@ import { CreatorJoinForm } from '@/components/creator-join-form';
 import { useLanguage } from '@/context/language-context';
 
 export default function CreatorJoinPage() {
-  const { dir } = useLanguage();
+  const { dir, language } = useLanguage();
   return (
-    <div dir={dir} className="relative flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-6 lg:p-8">
+    <div
+      dir={dir}
+      className={`relative flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-6 lg:p-8 ${
+        language === 'AR' ? 'font-arabic' : ''
+      }`}
+    >
       <div className="w-full max-w-2xl">
         <CreatorJoinForm />
       </div>
